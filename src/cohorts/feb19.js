8aacf1115b310d9d36cd4a9e1f1fa9430842b1f2
Ian King <ian.antking@gmail.com>
2019-04-09 19:19:28 +0100

    added feb19 cohort names

---
+++ src/cohorts/feb19.js
@@ -0,0 +1,17 @@
+const feb19 = [
+  'Nesu11',
+  'MuhammedAliGhori',
+  'dede79',
+  'gza007',
+  'jackpearsall',
+  'leesyski',
+  'Mike-Huggins',
+  'Richard-Pentecost',
+  'ruzla',
+  'joaquinox',
+  'tbilbe',
+  'sth289',
+  'thepunkyone',
+];
+
+module.exports = feb19;
