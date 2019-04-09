403c9950bcd2a7537931534a65d92e4089570c37
Ian King <ian.antking@gmail.com>
2019-04-09 19:32:28 +0100

    changed start script

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const feb19 = require('./cohorts/feb19');
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
+
+
+app.get('/', (_, res) => {
+  res.status(200).json({ Feb19: feb19 });
+});
 
 module.exports = app;
