8ed2249357ac84f9479b413fda2d57137ef3c9d1
Ian King <ian.antking@gmail.com>
2019-04-10 14:28:19 +0100

    added leaderboard data ffile

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const app = require('./src/app');
 const port = process.env.PORT || 4000;
 
 app.listen(port);
+console.log(`app listening on ${port}`);
--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const Axios = require('axios');
+
 const express = require('express');
 const feb19 = require('./cohorts/feb19');
 
@@ -7,7 +9,21 @@ app.use(express.json());
 
 
 app.get('/', (_, res) => {
-  res.status(200).json({ Feb19: feb19 });
+  const getAllFebStudentsProfiles = feb19.map(student => {
+    const url = `https://api.github.com/users/${student}/events`;
+    return Axios.get(url, 
+      {
+        auth: {
+          username: 'ian-antking',
+          password: process.env.TOKEN,
+        },
+      }
+    ).then(response => response.data).catch(error => console.log(error));
+  });
+
+  Promise.all(getAllFebStudentsProfiles).then(data => {
+    res.status(200).json({ feb19: data });
+  });
 });
 
 module.exports = app;
