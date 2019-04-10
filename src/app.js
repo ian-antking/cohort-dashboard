a6f83a8fe6e8337bec5fa7dbcf3003bf3fc6bf83
Ian King <ian.antking@gmail.com>
2019-04-10 23:24:54 +0100

    users/:name returns repo data

--- src/app.js
+++ src/app.js
@@ -1,20 +1,53 @@
 const profiles = require('./cohorts/feb19-profiles.json');
+const repos = require('./cohorts/feb19-repos.json');
 const express = require('express');
 const feb19 = require('./cohorts/feb19');
+const Axios = require('axios');
 
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
 
+const flatProfiles = repos.feb19.reduce((a, b) => {
+  return a.concat(b);
+});
+
 app.get('/users', (_, res) => {
   res.status(200).json({ Feb19: feb19 });
+});
+
+app.get('/repos', (_, res) => {
+  const getAllFebStudentsProfiles = feb19.map(student => {
+    const url = `https://api.github.com/users/${student}/repos`;
+    return Axios.get(
+      url,
+      {
+        auth: {
+          username: 'ian-antking',
+          password: process.env.TOKEN,
+        },
+      }
+    ).then(response => response.data);
+  });
+
+  Promise.all(getAllFebStudentsProfiles).then(data => {
+    res.status(200).json({ feb19: data });
+  });
 });
 
 app.get('/users/:name', (req, res) => {
   const profile = profiles.feb19.find(user => {
     return user.login.toLowerCase() === req.params.name.toLowerCase();
   });
-  res.status(200).json({ profile: profile });
+
+  const userRepos = flatProfiles.filter(repo => {
+    return repo.owner.login.toLowerCase() === req.params.name.toLowerCase();
+  });
+
+  res.status(200).json({
+    profile: profile,
+    repos: userRepos,
+  });
 });
 
 module.exports = app;
