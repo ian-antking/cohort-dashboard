79fa4c0b2a2f76ffc0eac46b8a56b48967c609d1
Ian King <ian.antking@gmail.com>
2019-04-10 23:58:35 +0100

    removed github api call

--- src/app.js
+++ src/app.js
@@ -21,25 +21,6 @@ app.get('/users', (_, res) => {
   res.status(200).json({ Feb19: feb19 });
 });
 
-app.get('/activity', (_, res) => {
-  const getAllFebStudentsProfiles = feb19.map(student => {
-    const url = `https://api.github.com/users/${student}/events`;
-    return Axios.get(
-      url,
-      {
-        auth: {
-          username: 'ian-antking',
-          password: process.env.TOKEN,
-        },
-      }
-    ).then(response => response.data);
-  });
-
-  Promise.all(getAllFebStudentsProfiles).then(data => {
-    res.status(200).json({ feb19: data });
-  });
-});
-
 app.get('/users/:name', (req, res) => {
   const profile = profiles.feb19.find(user => {
     return user.login.toLowerCase() === req.params.name.toLowerCase();
